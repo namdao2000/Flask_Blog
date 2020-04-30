5915e13f1e16edfc120ed1a59b975bcbb877c1e4
Nam <namdao2000@gmail.com>
2020-04-30 23:05:21 +1000

    Update

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
-from flask import Flask
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
-
+posts = [
+    {
+        'author': 'Nam Dao',
+        'title': "Blog Post 1",
+        'content': "First post content",
+        'date_posted': "April 20, 2018"
+    },
+    {
+        'author': 'Dianna Doe',
+        'title': "Blog Post 2",
+        'content': "Second post content",
+        'date_posted': "April 21, 2018"
+    }
+]
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/home')
+def home():
+    return render_template('home.html', posts=posts)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
